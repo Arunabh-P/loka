c0cfda5c34ae8bde80ab89c9a3bdcfa32cefffc0
Arunabh-P <arunabh1995@gmail.com>
2022-05-16 06:16:17 +0100

    second

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -3,12 +3,14 @@ import React from 'react'
 function Home() {
   return (
     <div className='hero'>
-        <div class="card bg-dark text-white">
-  <img src="/assets/bg.jpg"s class="card-img" alt="..." />
-  <div class="card-img-overlay">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-    <p class="card-text">Last updated 3 mins ago</p>
+        <div class="card bg-dark text-white border-0">
+  <img src="/assets/bg.jpg"s class="card-img" alt="background" height={"550px"} />
+  <div class="card-img-overlay d-flex flex-column justify-content-center">
+    <div className="container">
+
+    <h5 class="card-title display-3 fw-bolder mb-0">NEW SEASON ARRIVALS</h5>
+    <p class="card-text lead fs-2">CHECK OUT ALL THE TRENDS</p>
+    </div>
   </div>
 </div>
 
